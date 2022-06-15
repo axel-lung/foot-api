c6353b0adf1cd84e3adfc4306b4f7f45a05f4ba8
axel-lung <axel.lung@outlook.fr>
2022-06-15 23:31:53 +0200

    first commit

---
+++ src/Entity/User.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Action\NotFoundAction;
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Controller\MeController;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\User\JWTUserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ApiResource(
+    security: 'is_granted("ROLE_USER")',
+    collectionOperations: [
+        'me' => [
+            'pagination_enable' => false,
+            'path' => '/me',
+            'method' => 'get',
+            'controller' => MeController::class,
+            'read' => false,
+            'openapi_context' => [
+                'security' => [['bearer' => []]]
+            ]
+        ]
+    ],
+    itemOperations: [
+        'get' => [
+            'controller' => NotFoundAction::class,
+            'openapi_context' => ['summary' => 'hidden'],
+            'read' => false,
+            'output' => false
+        ]
+        ],
+        normalizationContext: ['groups' => ['read:User']]
+)]
+class User implements UserInterface, PasswordAuthenticatedUserInterface, JWTUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[Groups(['read:User'])]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    #[Groups(['read:User'])]
+    private $email;
+
+    #[ORM\Column(type: 'json')]
+    #[Groups(['read:User'])]
+    private $roles = [];
+
+    #[ORM\Column(type: 'string')]
+    private $password;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public static function createFromPayload($id, array $payload)
+    {
+        return (new User())->setId($id)->setEmail($payload['username'] ?? '');
+    }
+}
---
+++ src/OpenApi/OpenApiFactory.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\OpenApi;
+
+use ApiPlatform\Core\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\Core\OpenApi\Model\Operation;
+use ApiPlatform\Core\OpenApi\Model\PathItem;
+use ApiPlatform\Core\OpenApi\Model\RequestBody;
+use ApiPlatform\Core\OpenApi\OpenApi;
+use ArrayObject;
+
+class OpenApiFactory implements OpenApiFactoryInterface
+{
+
+
+    public function __construct(private OpenApiFactoryInterface $decorated)
+    {
+        
+    }
+
+    public function __invoke(array $context = []): OpenApi
+    {
+        $openApi = $this->decorated->__invoke($context);
+        /** @var PathItem $path */
+        foreach ($openApi->getPaths()->getPaths() as $key => $path) {
+            if($path->getGet() && $path->getGet()->getSummary() === 'hidden'){
+                $openApi->getPaths()->addPath($key, $path->withGet(null));
+            }
+        }
+
+        $schemas = $openApi->getComponents()->getSchemas();
+        $schemas['bearerAuth'] = new ArrayObject([
+            'type' => 'http',
+            'scheme' => 'bearer',
+            'format' => 'JWT'
+        ]);
+        
+        $schemas = $openApi->getComponents()->getSecuritySchemes();
+        $schemas['cookieAuth'] = new \ArrayObject([
+            'type' => 'apiKey',
+            'in' => 'cookie',
+            'name' => 'PHPSESSID'
+        ]);
+
+        $schemas = $openApi->getComponents()->getSchemas();
+        $schemas['Credentials'] = new \ArrayObject([
+            'type' => 'object',
+            'properties' => [
+                'username' => [
+                    'type' => 'string',
+                    'example' => 'name@domain.com'
+                ],
+                'password' => [
+                    'type' => 'string',
+                    'example' => '0000'
+                ]
+            ]
+        ]);
+
+        $schemas['Token'] = new \ArrayObject([
+            'type' => 'object',
+            'properties' => [
+                'token' => [
+                    'type' => 'string',
+                    'readOnly' => true,
+                ],
+            ]
+        ]);
+
+        $pathItem = new PathItem(
+            post: new Operation(
+                operationId: 'postApiLogin',
+                tags: ['Auth'],
+                requestBody: new RequestBody(
+                    content: new \ArrayObject([
+                        'application/json' => [
+                            'schemas' => [
+                                '$ref' => '#/components/schemas/Credentials'
+                            ]
+                        ]
+                    ])
+                ),
+                responses: [
+                    '200' => [
+                        'description' => 'Token JWT',
+                        'content' => [
+                            'application/json' => [
+                                'schema' => [
+                                    '$ref' => '#/components/schemas/Token'
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            )
+        );
+
+        $openApi->getPaths()->addPath('/api/login', $pathItem);
+
+        $openApi = $openApi->withSecurity(['cookieAuth' => []]);
+
+        return $openApi;
+    }
+}
